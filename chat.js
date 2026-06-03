950afdbb33bb01bb418c994b3f6977941b4bb82b
Pradeep09-10-99 <homesmeena5536@gmail.com>
2026-06-03 18:19:30 +0530

    Update chat.js

--- chat.js
+++ chat.js
@@ -8,9 +8,8 @@ export const handler = async (event) => {
     const body = JSON.parse(event.body);
     const userMessage = body.message;
     
-    // Netlify environment variable se API key fetch hogi (Frontend par kisi ko nahi dikhegi)
-    const apiKey = AIzaSyAX_1BvyJKFLjXPO_H6GuWacvHTLKqmTBU; 
-
+ // Netlify environment variable se API key fetch hogi (Frontend par kisi ko nahi dikhegi)
+const apiKey = "AIzaSyAX_1BvyJKFLjXPO_H6GuWacvHTLKqmTBU";
     // Google Gemini API ko request bhejna
     const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${apiKey}`, {
       method: "POST",
